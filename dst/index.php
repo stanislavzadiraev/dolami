14ad1f4abad7f97bf2f68ceb2a1c4ca1f3437e8c
stanislavzadiraev <zadiraeff@yandex.ru>
2024-07-25 20:05:22 +0300

    null
    Signed-off-by: stanislavzadiraev <zadiraeff@yandex.ru>

--- dst/index.php
+++ dst/index.php
@@ -3,105 +3,152 @@
 include 'client.php';
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// общее 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function test($key){
-    if ( isset($_GET) && isset($_GET[$key]) )
-        return strval($_GET[$key]);
-    else if ( isset($_POST) && isset($_POST[$key]) )
-        return strval($_POST[$key]);
-    else
-        return '';
+function takeargs($server){
+    switch($server['REQUEST_METHOD']){
+        case 'GET': return $_GET;
+        case 'POST': return $_POST;
+    };
+
+    return [];
 }
 
-function offile($name) {
-    $handle = fopen($name, "r");
-    $contents = fread($handle, filesize($name));
-    fclose($handle);
-
-    return $contents;
+function fileread($name){
+    return implode("\n", file($name));
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-function form() {echo(' 
-    <form action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'" method="POST">
-      <h3>Заявка</h3>
-
-      <h3>Заявление</h3>
-        <label for="id">id:</label>
-        <input type="text" name="id" value="'.test("id").'"><br>
-
-        <label for="amount">amount:</label>
-        <input type="text" name="amount" value="'.test("amount").'"><br>
-
-        <label for="prepaid">prepaid:</label>
-        <input type="text" name="prepaid" value="'.test("prepaid").'"><br>
-
-        <label for="items">items:</label>
-        <input type="text" name="items" value="'.test("items").'"><br>
-
-      <h3>Заявитель</h3>
-        <label for="first_name">first_name:</label>
-        <input type="text" name="first_name" value="'.test("first_name").'"><br>
-
-        <label for="last_name">last_name:</label>
-        <input type="text" name="last_name" value="'.test("last_name").'"><br>
-
-        <label for="phone">phone:</label>
-        <input type="text" name="phone" value="'.test("phone").'"><br>
-
-        <label for="email">email:</label>
-        <input type="text" name="email" value="'.test("email").'"><br>        
-
-      <input type="submit" value="Отправить">
-    </form> 
-    ');};
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// формы
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-if (
-        $_SERVER['REQUEST_METHOD'] === 'POST' && 
-        strlen(test("id")) && strlen(test("amount")) && strlen(test("prepaid")) && strlen(test("items")) &&
-        strlen(test("first_name")) && strlen(test("last_name")) && strlen(test("phone")) && strlen(test("email"))
-    ) 
-    {
+function testargs($args, $form){
+    foreach($form as $item => $data)
+        if (!isset($args[$item]) || !$data['test']($args[$item]))
+            return false;
+
+    return true;
+}
+
+function fillform($form, $args){
+    $result = '';
+
+    $result .= '<form action="'.htmlspecialchars($_SERVER["SCRIPT_NAME"]).'" method="POST">';
+
+    foreach($form as $item => $data){
+        $value = '';
+        if (isset($args[$item]))
+            $value = $args[$item];
+        $result .= '<label for="'.$item.'">'.$data['name'].':</label>';
+        $result .= '<input name="'.$item.'" type="'.$data['type'].'" value="'.$value.'"><br>';
+    };
+
+    $result .= '<input type="submit" value="Отправить"></form>';
+    
+    return $result;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// долями, формы
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function test($data){
+    return strlen($data);
+}
+
+$form = [
+    'id' => [
+        'name' => 'Номер Заказа',
+        'test' => 'test',
+        'type' => 'text',
+    ],
+    'amount' => [
+        'name' => 'Сумма',
+        'test' => 'test',
+        'type' => 'number',
+    ],
+    'prepaid' => [
+        'name' => 'Скидка',
+        'test' => 'test',
+        'type' => 'number',
+    ],
+    'items' => [
+        'name' => 'Позиции',
+        'test' => 'test',
+        'type' => 'text',
+    ],
+    'first_name' => [
+        'name' => 'Имя',
+        'test' => 'test',
+        'type' => 'text',
+    ],
+    'last_name' => [
+        'name' => 'Фамилия',
+        'test' => 'test',
+        'type' => 'text',
+    ],
+    'phone' => [
+        'name' => 'Телефоный номер',
+        'test' => 'test',
+        'type' => 'tel',
+    ],
+    'email' => [
+        'name' => 'Почтовый ящик',
+        'test' => 'test',
+        'type' => 'email',
+    ],
+];
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// долями, клиент
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function client($args){
+    try{
         $client = new Client(
-            offile(realpath('./../' . 'sequre/user')),
-            offile(realpath('./../' . 'sequre/word')
+            fileread(realpath('./../' . 'sequre/user')),
+            fileread(realpath('./../' . 'sequre/word')
         ));
         $client->setCertPath(realpath('./../' . 'sequre/certificate.pem'));
         $client->setKeyPath(realpath('./../' . 'sequre/private.key'));
 
-        echo "client:<br>"; print_r($client); echo "<br>";
-
         $data = [
             'order' => [
-                'id' => $_POST['id'],
-                'amount' => $_POST['amount'],
-                'prepaid_amount' => $_POST['prepaid'],
-                'items' => $_POST['items'],
+                'id' => $args['id'],
+                'amount' => $args['amount'],
+                'prepaid_amount' => $args['prepaid'],
+                'items' => $args['items'],
             ],
             'client_info' => [
-                'first_name' => $_POST['first_name'],
-                'last_name' => $_POST['last_name'],
-                'phone' => $_POST['phone'],
-                'email' => $_POST['email'],
+                'first_name' => $args['first_name'],
+                'last_name' => $args['last_name'],
+                'phone' => $args['phone'],
+                'email' => $args['email'],
             ],
             'notification_url' => 'note',
             'fail_url' => 'fail',
             'success_url' => 'done',
         ];
 
-        try{
-            $response = $client->create($data);
+        return $client->create($data);
 
-            echo "responce:<br>"; print_r($response); echo "<br>";
-        }
-        catch (Exception $e) {
-            echo "exception:<br>"; print_r($e); echo "<br>";
-        }
     }
-else
-    form();
+    catch (Exception $e) {
+        echo "client:<br>"; print_r($client); echo "<br>";
+        echo "exception:<br>"; print_r($e); echo "<br>";
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// частное
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+if(!testargs(takeargs($_SERVER), $form)){
+    print(fillform($form, takeargs($_SERVER)));
+}else{
+    print(client(takeargs($_SERVER)));
+};
+
+
 ?>
